9e12eb36ec34ffd8f41c70e929a3280c67641c91
favu100 <Fabian.Vu@hhu.de>
2016-06-23 23:27:23 +0200

    Update Main Program

--- src/main/java/de/hhu/propra16/coastal/tddt/TDDTMenu.java
+++ src/main/java/de/hhu/propra16/coastal/tddt/TDDTMenu.java
@@ -3,8 +3,15 @@ package de.hhu.propra16.coastal.tddt;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import javax.xml.soap.Text;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 
 public class TDDTMenu {
@@ -21,19 +28,60 @@ public class TDDTMenu {
     MenuItem miclose;
 
     @FXML
+    TextArea taeditor;
+
+    @FXML
+    TextArea tatest;
+
+    @FXML
     public void open(ActionEvent event) {
         FileChooser dialog = new FileChooser();
         dialog.setTitle("Wähle eine Datei aus");
         dialog.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML Files", "*.xml"));
         dialog.showOpenDialog(primaryStage);
+        Path p = Paths.get("../Software.java");
+        try {
+            if (!Files.exists(p)) {
+                Files.write(p, new byte[0], StandardOpenOption.CREATE);
+            } else {
+                taeditor.setText(new String(Files.readAllBytes(p)));
+            }
+        } catch (Exception e) {
+            System.err.println("Write to data failed");
+        }
+
+        Path p2 = Paths.get("../SoftwareTest.java");
+        try {
+            if(!Files.exists(p2)) {
+                Files.write(p2, new byte[0], StandardOpenOption.CREATE);
+            } else {
+                tatest.setText(new String(Files.readAllBytes(p2)));
+            }
+        } catch (Exception e) {
+            System.err.println("Write to data failed");
+        }
     }
 
     @FXML
     public void save(ActionEvent event) {
-        FileChooser dialog = new FileChooser();
+        /*FileChooser dialog = new FileChooser();
         dialog.setTitle("Wähle eine Datei aus");
         dialog.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML Files", "*.xml"));
-        dialog.showOpenDialog(primaryStage);
+        dialog.showSaveDialog(primaryStage);*/
+
+        Path p = Paths.get("../Software.java");
+        try {
+            Files.write(p, taeditor.getText().getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (Exception e) {
+            System.err.println("Write to data failed");
+        }
+
+        Path p2 = Paths.get("../SoftwareTest.java");
+        try {
+            Files.write(p2, tatest.getText().getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (Exception e) {
+            System.err.println("Write to data failed");
+        }
     }
 
     @FXML
