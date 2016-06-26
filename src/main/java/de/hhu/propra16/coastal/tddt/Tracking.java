03bf464ebfb7ba74ffd0126c7c1f102a19349b9f
JimmyDuong97 <maduo100@uni-duesseldorf.de>
2016-06-26 19:17:19 +0200

    Add Tracking Created Datatype for counting time

--- src/main/java/de/hhu/propra16/coastal/tddt/Tracking.java
+++ src/main/java/de/hhu/propra16/coastal/tddt/Tracking.java
@@ -1,7 +1,28 @@
 package de.hhu.propra16.coastal.tddt;
-
 /**
  * Created by Jimmy on 26.06.2016.
  */
 public class Tracking {
+    int timeSpend;
+    long start;
+    long end;
+    public Tracking(){
+    }
+
+    public void startTracking() {
+        start = System.currentTimeMillis();
+    }
+
+    public void endTracking(){
+        end = System.currentTimeMillis();
+    }
+
+    public int returnTimeSpentinSeconds() {
+        timeSpend = (int) (end-start)/1000;
+        return timeSpend;
+    }
+
+
+
+
 }
