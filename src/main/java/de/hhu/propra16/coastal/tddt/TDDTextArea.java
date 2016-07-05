c448056df603c9f4618640a60d1f5618a6752916
JimmyDuong97 <maduo100@uni-duesseldorf.de>
2016-07-06 01:31:22 +0200

    Implementing RichtTextFX BETA

--- src/main/java/de/hhu/propra16/coastal/tddt/TDDTextArea.java
+++ src/main/java/de/hhu/propra16/coastal/tddt/TDDTextArea.java
@@ -1,7 +1,45 @@
 package de.hhu.propra16.coastal.tddt;
 
-import javafx.scene.control.TextArea;
+//import javafx.scene.control.TextArea;
+import java.util.*;
+import javafx.*;
+import org.fxmisc.flowless.*;
+import org.fxmisc.richtext.*;
 
-public class TDDTextArea extends TextArea implements ITDDTextArea {
+public class TDDTextArea extends CodeArea implements ITDDTextArea {
+    String[] KEYWORDS = {"abstract", "assert", "boolean", "break", "byte",
+            "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else",
+            "enum", "extends", "final", "finally", "float",
+            "for", "goto", "if", "implements", "import",
+            "instanceof", "int", "interface", "long", "native",
+            "new", "package", "private", "protected", "public",
+            "return", "short", "static", "strictfp", "super",
+            "switch", "synchronized", "this", "throw", "throws",
+            "transient", "try", "void", "volatile", "while"};
 
+    public void setText(String text){
+
+    }
+
+    /*
+    private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
+    private static final String PAREN_PATTERN = "\\(|\\)";
+    private static final String BRACE_PATTERN = "\\{|\\}";
+    private static final String BRACKET_PATTERN = "\\[|\\]";
+    private static final String SEMICOLON_PATTERN = "\\;";
+    private static final String STRING_PATTERN = "\"([^\"\\\\]|\\\\.)*\"";
+    private static final String COMMENT_PATTERN = "//[^\n]*" + "|" + "/\\*(.|\\R)*?\\*/";
+
+    /*
+    private static final Pattern PATTERN = Pattern.compile(
+            "(?<KEYWORD>" + KEYWORD_PATTERN + ")"
+                    + "|(?<PAREN>" + PAREN_PATTERN + ")"
+                    + "|(?<BRACE>" + BRACE_PATTERN + ")"
+                    + "|(?<BRACKET>" + BRACKET_PATTERN + ")"
+                    + "|(?<SEMICOLON>" + SEMICOLON_PATTERN + ")"
+                    + "|(?<STRING>" + STRING_PATTERN + ")"
+                    + "|(?<COMMENT>" + COMMENT_PATTERN + ")"
+    );
+    */
 }
