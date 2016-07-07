e734b8b40339fad9f86ad7d206c6f4cf65227ff7
mawen7 <mawen7@users.noreply.github.com>
2016-07-07 09:27:57 +0200

    fix build (TDDTextArea)

--- src/main/java/de/hhu/propra16/coastal/tddt/TDDTextArea.java
+++ src/main/java/de/hhu/propra16/coastal/tddt/TDDTextArea.java
@@ -1,13 +1,12 @@
 package de.hhu.propra16.coastal.tddt;
 
 //import javafx.scene.control.TextArea;
-import java.util.*;
-import javafx.*;
-import org.fxmisc.flowless.*;
-import org.fxmisc.richtext.*;
+import org.fxmisc.richtext.CodeArea;
+
+import java.util.regex.Pattern;
 
 public class TDDTextArea extends CodeArea implements ITDDTextArea {
-    String[] KEYWORDS = {"abstract", "assert", "boolean", "break", "byte",
+    private static final String[] KEYWORDS = {"abstract", "assert", "boolean", "break", "byte",
             "case", "catch", "char", "class", "const",
             "continue", "default", "do", "double", "else",
             "enum", "extends", "final", "finally", "float",
@@ -19,10 +18,10 @@ public class TDDTextArea extends CodeArea implements ITDDTextArea {
             "transient", "try", "void", "volatile", "while"};
 
     public void setText(String text){
-
+        clear();
+        appendText(text);
     }
 
-    /*
     private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
     private static final String PAREN_PATTERN = "\\(|\\)";
     private static final String BRACE_PATTERN = "\\{|\\}";
@@ -31,7 +30,6 @@ public class TDDTextArea extends CodeArea implements ITDDTextArea {
     private static final String STRING_PATTERN = "\"([^\"\\\\]|\\\\.)*\"";
     private static final String COMMENT_PATTERN = "//[^\n]*" + "|" + "/\\*(.|\\R)*?\\*/";
 
-    /*
     private static final Pattern PATTERN = Pattern.compile(
             "(?<KEYWORD>" + KEYWORD_PATTERN + ")"
                     + "|(?<PAREN>" + PAREN_PATTERN + ")"
@@ -41,5 +39,4 @@ public class TDDTextArea extends CodeArea implements ITDDTextArea {
                     + "|(?<STRING>" + STRING_PATTERN + ")"
                     + "|(?<COMMENT>" + COMMENT_PATTERN + ")"
     );
-    */
 }
