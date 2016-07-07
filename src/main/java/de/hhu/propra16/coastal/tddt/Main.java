d95840f8744e9fc433007fa08a584c51b25db196
favu100 <Fabian.Vu@hhu.de>
2016-07-07 18:55:20 +0200

    Fix Merge conflict

--- src/main/java/de/hhu/propra16/coastal/tddt/Main.java
+++ src/main/java/de/hhu/propra16/coastal/tddt/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 import java.net.URL;
 
 public class Main extends Application {
-
+    //test
     @Override
     public void start(Stage primaryStage) {
         Parent root = null;
